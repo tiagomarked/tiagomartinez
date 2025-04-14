abe916f1583e6f9fb624a0298ac755ded5385904
Tiago Martinez <tmfernandes002@gmail.com>
2025-04-14 22:59:56 +0100

    Add deployment automation

---
+++ deploy.js
@@ -0,0 +1,22 @@
+require("dotenv").config({ "path": "deploy.env" });
+
+const FtpDeploy = require("ftp-deploy");
+const ftpDeploy = new FtpDeploy();
+
+console.log(process.env);
+
+const config = {
+    user: process.env.FTP_USER,
+    password: process.env.FTP_PASSWORD,
+    host: process.env.FTP_HOST,
+    port: process.env.FTP_PORT,
+    localRoot: __dirname + "/dist",
+    remoteRoot: process.env.FTP_REMOTE_PATH,
+    include: ["*", "**/*"],
+    deleteRemote: true,
+};
+
+ftpDeploy
+    .deploy(config)
+    .then(res => console.log("Deployed:", res))
+    .catch(err => console.error("Error:", err));
